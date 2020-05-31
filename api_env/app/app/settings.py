7d9c393361b7746242905f469927db74a0a3c319
Anurag Timilsina <anurag.timilsina@gmail.com>
2020-05-31 10:43:30 +0545

    User API created

--- api_env/app/app/settings.py
+++ api_env/app/app/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +80,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
---
+++ api_env/app/user/views.py
@@ -0,0 +1,7 @@
+from user.serializers import UserSerializer
+
+from rest_framework import generics
+
+class CreateUserView(generics.CreateAPIView):
+    """ Create a new user in the system """
+    serializer_class = UserSerializer
